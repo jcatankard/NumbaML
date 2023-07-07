39656e687f40e8988fbe463e597ae3362b17824c
e10110199 <joshua.tankard@vista.com>
2023-07-07 13:37:47 +0200

    add long_description_content_type='text/markdown' to setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,6 @@ setup(
     author='Josh Tankard',
     description='Linear regression with Numba',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     install_requires=['numba', 'numpy', 'scipy']
 )
