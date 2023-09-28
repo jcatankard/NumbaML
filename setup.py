760e23e27369eb1c8ca3798da3126a99225f9953
e10110199 <joshua.tankard@vista.com>
2023-09-28 09:30:19 +0200

    update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='NumbaML',
-    version='1.0.6',
+    version='1.0.7',
     packages=['numbaml'],
     url='https://github.com/jcatankard/NumbaML',
     author='Josh Tankard',
